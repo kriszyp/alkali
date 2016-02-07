72d8d673b329902d29cab6f809bee1b2fd04ffff
Kris Zyp <kriszyp@gmail.com>
2016-02-06 17:38:17 -0700

    Update original object, and avoid duplicate objects, instead of creating new objects

--- Copy.js
+++ Copy.js
@@ -1,27 +1,46 @@
 define(['./Variable', './lang'],
 		function(Variable, lang){
 
-	function deepCopy(source) {
+	function deepCopy(source, target, derivativeMap) {
 		if(source && typeof source == 'object'){
 			if(source instanceof Array){
-				var target = [];
-				for(var i = 0, l = source.length; i < l; i++){
-					target[i] = deepCopy(source[i]);
+				if(target instanceof Array){
+					// truncate as necessary
+					if (target.length > source.length) {
+						target.splice(source.length, target.length - source.length)
+					}
+				} else {
+					target = derivativeMap && derivativeMap.get(source);
+					if (!target) {
+						target = [];
+						derivativeMap && derivativeMap.set(source, target);
+					}
 				}
-			}else{
-				var target = {};
+				for(var i = 0, l = source.length; i < l; i++){
+					target[i] = deepCopy(source[i], target[i], derivativeMap);
+				}
+			}else {
+				if (!target || typeof target !== 'object') {
+					target = derivativeMap && derivativeMap.get(source);
+					if (!target) {
+						target = {};
+						derivativeMap && derivativeMap.set(source, target);
+					}
+				}
 				for(var i in source){
-					target[i] = deepCopy(source[i]);
+					target[i] = deepCopy(source[i], target[i], derivativeMap);
 				}
 			}
 			return target;
 		}
 		return source;
 	}
-	return lang.compose(Variable, function(copiedFrom){
+
+	var Copy = lang.compose(Variable, function(copiedFrom){
 		// this is the variable that we derive from
 		this.copiedFrom = copiedFrom;
 		this.derivativeMap = new lang.WeakMap(null, 'derivative');
+		this.isDirty = new Variable(false)
 	}, {
 		valueOf: function(context){
 			if(this.state){
@@ -31,7 +50,7 @@ define(['./Variable', './lang'],
 			if(value && typeof value == 'object'){
 				var derivative = this.derivativeMap.get(value);
 				if (derivative == null){
-					this.derivativeMap.set(value, derivative = deepCopy(value));
+					this.derivativeMap.set(value, derivative = deepCopy(value, undefined, this.derivativeMap));
 				}
 				return derivative;
 			}
@@ -41,9 +60,38 @@ define(['./Variable', './lang'],
 			}
 			return thisValue;
 		},
+		getCopyOf: function(value){
+			var derivative = this.derivativeMap.get(value);
+			if (derivative == null){
+				this.derivativeMap.set(value, derivative = deepCopy(value, undefined, this.derivativeMap));
+			}
+			return derivative;
+		},
 		save: function(){
-			// TODO: copy instead?
-			this.copiedFrom.put(this.valueOf());
+			// copy back to the original object
+			var original = this.copiedFrom.valueOf()
+			var newCopiedFrom = deepCopy(this.valueOf(), original);
+			if (original !== newCopiedFrom) {
+				// if we have replaced it with a new object/value, put it
+				this.copiedFrom.put && this.copiedFrom.put(newCopiedFrom);
+			} else {
+				// else we have modified an existing object, but we still need to notify
+				if (this.copiedFrom.invalidate) { // copiedFrom doesn't have to be a variable, it can be a plain object
+					this.copiedFrom.invalidate();
+				}
+			}
+			this.isDirty.put(false)
+			this.onSave && this.onSave()
+		},
+		revert: function(){
+			var original = this.copiedFrom.valueOf();
+			deepCopy(original, this.derivativeMap.get(value), this.derivativeMap);
+			this.isDirty.put(false)
+		},
+		invalidate: function(){
+			this.isDirty.put(true)
+			return Variable.prototype.invalidate.apply(this, arguments);
 		}
 	});
+	return Copy;
 });
