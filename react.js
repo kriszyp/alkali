5be89062ce4b8f8e79ebbdd7e34cc3ca829fbfab
Kris Zyp <kriszyp@gmail.com>
2016-03-16 22:40:42 -0600

    Add support for promises in generator wrapper

--- react.js
+++ react.js
@@ -1,30 +1,61 @@
 define(['./lang', './Variable'], function(lang, Variable) {
+	var getValue
 	var GeneratorVariable = lang.compose(Variable.Composite, function Call(generator){
 		this.generator = generator
 	}, {
 		init: function() {
 			this.args = []
 		},
-		getValue: function(context) {
+		getValue: getValue = function(context, resuming) {
 			var lastValue
-			var generation = this.generator()
-			var i = 0
+			var i
+			var generatorIterator
+			if (resuming) {
+				// resuming from a promise
+				generatorIterator = resuming.iterator
+				i = resuming.i
+				lastValue = resuming.value
+			} else {
+				// a fresh start
+				i = 0
+				generatorIterator = this.generator()				
+			}
+			
 			var args = this.args
 			do {
-				var stepReturn = generation.next(lastValue)
+				var stepReturn = generatorIterator.next(lastValue)
 				if (stepReturn.done) {
 					return stepReturn.value
 				}
 				var nextVariable = stepReturn.value
+				// compare with the arguments from the last
+				// execution to see if they are the same
 				if (args[i] !== nextVariable) {
 					if (args[i]) {
 						args[i].unsubscribe(this)
 					}
-					nextVariable.subscribe(this)
-					this.args[i] = nextVariable
+					// subscribe if it is a variable
+					if (nextVariable && nextVariable.subscribe) {
+						nextVariable.subscribe(this)
+						this.args[i] = nextVariable
+					} else {
+						this.args[i] = null
+					}
 				}
 				i++
-				lastValue = nextVariable.valueOf()
+				lastValue = nextVariable && nextVariable.valueOf()
+				if (lastValue && lastValue.then) {
+					// if it is a promise, we will wait on it
+					var variable = this
+					// and return the promise so that the getValue caller can wait on this
+					return lastValue.then(function(value) {
+						return getValue.call(this, context, {
+							i: i,
+							iterator: generatorIterator,
+							value: value
+						})
+					})
+				}
 			} while(true)
 		}
 	})
