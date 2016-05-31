00fd8fdaf5b218291ef03072ab00d5603fa7f7cb
Kris Zyp <kriszyp@gmail.com>
2016-05-31 15:14:47 -0600

    Add Copy add Updaters to main exports

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-define(['./Element', './Variable', './react', './Updater', './operators'], function(Element, Variable, react, Updater, operators) {
+define(['./Element', './Variable', './react', './Updater', './operators', './Copy'], function(Element, Variable, react, Updater, operators, Copy) {
 	var main = Object.create(Element)
+  main.Copy = Copy
 	main.Element = Element
 	main.Variable = Variable
 	main.all = Variable.all
 	main.react = react
 	main.Updater = Updater
+  Object.assign(main, Updater)
 	Object.assign(main, operators)
 	return main
 })
