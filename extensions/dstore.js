abca3c7d1379fbde38c0b586ac8a30afc6dae29e
Aaron Hamid <aaron.hamid@gmail.com>
2017-01-10 14:31:14 -0500

    dstore: use single last value to determine idempotency

--- extensions/dstore.js
+++ extensions/dstore.js
@@ -5,18 +5,18 @@ define(['../util/lang', '../Variable'], function(lang, Variable){
 	{
 		gotValue: function(value, context) {
 			value = Variable.prototype.gotValue.call(this, value, context)
-			if (value && value.on && !(this.seen || (this.seen = new Set())).has(value)){
+			if (value && value.on && !(this._lastDSValue === value)){
 				// if it an object that can deliver notifications through `on` events, we listen for that (like dstore collections)
 				var variable = this
 				var handle = value.on(['add','update','delete'], function(event) {
 					event.visited = new Set()
 					variable.updated(event)
 				})
-				var seen = this.seen.add(value)
+				this._lastDSValue = value
 				this.returnedVariable = {
 					// remove the listener when we unsubscribe
 					stopNotifies: function() {
-						seen.delete(value)
+						variable._lastDSValue = undefined
 						handle.remove()
 					},
 					notifies: function(){}
