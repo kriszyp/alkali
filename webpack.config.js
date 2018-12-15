82c0021ae20dd403da788ab14a44d0317a5ec803
Kris Zyp <kriszyp@gmail.com>
2018-12-14 17:26:01 -0700

    update version and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@ var webpack = require('webpack')
 module.exports = {
     entry:  './index.js',
     output: {
-        filename: 'dist/index.js',
+        filename: './index.js',
         library: 'alkali',
         libraryTarget: 'umd'
     },
     plugins: [
       new webpack.optimize.UglifyJsPlugin()
     ],
+    mode: 'production',
     devtool: 'source-map'
 };
