a95fe1c487d6104ffae770137871315b75fa781b
Kris Zyp <kriszyp@gmail.com>
2019-04-19 17:11:49 -0600

    Clear context after async action finishes

--- extensions/async-hooks.js
+++ extensions/async-hooks.js
@@ -2,6 +2,7 @@ var async_hooks = require('async_hooks');
 var Variable = require('../Variable')
 
 var contexts = []
+//global.asyncEvents = []
 var previousContext
 module.exports.enable = () => {
 	const alkaliAsyncHook = async_hooks.createHook({
@@ -10,23 +11,27 @@ module.exports.enable = () => {
 				if (resource.isChainedPromise) {
 					let context = Variable.currentContext
 					if (context) {
-						contexts[asyncId] = context
-					}
+				    	contexts[asyncId] = context
+//				    	global.asyncEvents[asyncId] = [type + 'init ' + new Error().stack, context]
+				    }
 				}
 			}
-		},
-		before(asyncId) {
-			previousContext = Variable.currentContext
-			// we could potentially throw an error if there is an existing previousContext, since there should not be a global context
-			Variable.currentContext = contexts[asyncId];
-		},
-		after(asyncId) {
-			Variable.currentContext = previousContext
-			delete contexts[asyncId]
-		},
-		destroy(asyncId) {
-			delete contexts[asyncId]
-		}
+	    },
+	    before(asyncId) {
+	    	previousContext = Variable.currentContext
+	    	// we could potentially throw an error if there is an existing previousContext, since there should not be a global context
+	    	Variable.currentContext = contexts[asyncId];
+//	    	(global.asyncEvents[asyncId] || (global.asyncEvents[asyncId] = [])).push('before', previousContext)
+	    },
+	    after(asyncId) {
+	    	Variable.currentContext = undefined
+	    	delete contexts[asyncId]
+//	    	delete global.asyncEvents[asyncId]
+	    },
+	    destroy(asyncId) {
+	    	delete contexts[asyncId]
+//	    	delete global.asyncEvents[asyncId]
+	    }
 	})
 	alkaliAsyncHook.enable()
 }
